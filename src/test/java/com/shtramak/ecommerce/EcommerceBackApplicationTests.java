c6d70ff52bfd3cdbc6bf79ea2c7dbe65b0b9d1a5
Andrii Kovalchuk <shtramak@gmail.com>
2020-02-09 21:36:38 +0200

    initial commit

---
+++ src/test/java/com/shtramak/ecommerce/EcommerceBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shtramak.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
