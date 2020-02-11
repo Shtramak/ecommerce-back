3c5d79c78b73fb07be51772314cf8d3078a42053
andrii.kovalchuk <shtramak@gmail.com>
2020-02-11 12:32:37 +0200

    add @CrossOrigin annotation and change rest base-path to /api

--- src/main/java/com/shtramak/ecommerce/repository/ProductCategoryRepository.java
+++ src/main/java/com/shtramak/ecommerce/repository/ProductCategoryRepository.java
@@ -3,7 +3,9 @@ package com.shtramak.ecommerce.repository;
 import com.shtramak.ecommerce.model.ProductCategory;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @RepositoryRestResource(path = "product-category", collectionResourceRel = "productCategory")
 public interface ProductCategoryRepository extends CrudRepository<ProductCategory, Long> {
 }
--- src/main/java/com/shtramak/ecommerce/repository/ProductRepository.java
+++ src/main/java/com/shtramak/ecommerce/repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.shtramak.ecommerce.repository;
 
 import com.shtramak.ecommerce.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
